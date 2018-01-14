c5fd24e61ad5f0f728b467d3f6df8a6bf52063d1
mazbah932 <33983949+mazbah932@users.noreply.github.com>
2018-01-14 17:12:33 +0600

    Add files via upload

---
+++ slider/owl-carousel/js/main.js
@@ -0,0 +1,18 @@
+$('.single-slider').owlCarousel({
+    autoplayTimeout: 300,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
